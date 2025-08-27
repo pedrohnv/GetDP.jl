5c0385982a195e78787a21dff610dac27589e3fe
Pedro Henrique Nascimento Vieira <phnvieira@proton.me>
2025-08-27 14:18:38 -0300

    remove duplicated function

--- src/GetDP.jl
+++ src/GetDP.jl
@@ -16,10 +16,6 @@ include("getdp_object.jl")
 export AbstractGetDPObject, GetDPObject, SimpleItem, Base_, ObjectItem, CaseItem_, Case_
 export code, add_raw_code!, add_comment!, add!
 
-function get_getdp_executable()
-    return "getdp"
-end
-
 export get_getdp_executable
 
 # Include main components
